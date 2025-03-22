b033a2cc92ff8311aa3d21b354e185f9e7ff23f7
John Sokol <soksy@yahoo.com>
2025-03-22 18:00:09 +1100

    added wait_for_db and migrate django command invocations to docker-comnpose and checks GHA

--- app/core/management/commands/wait_for_db.py
+++ app/core/management/commands/wait_for_db.py
@@ -23,6 +23,6 @@ class Command(BaseCommand):
                 self.check(databases=['default'])
                 db_up = True
             except (Psycopg2Error, OperationalError):
-                self.stdout.write('Database unavailable, waiting 1 second...')
+                self.stdout.write(self.style.ERROR('Database unavailable, waiting 1 second...'))
                 time.sleep(1)
         self.stdout.write(self.style.SUCCESS('Database available!'))
